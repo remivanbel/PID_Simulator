e949ec91022ed40cbb4174e6e821cc21c9b60d76
Remi Van Belleghem <remi.vanbelleghem@gmail.com>
2023-11-10 20:44:48 +0100

    add addtional information

--- intro.py
+++ intro.py
@@ -7,4 +7,23 @@ st.set_page_config(
 
 st.write("# Welkom 👋")
 
+st.markdown( """
+Op deze website kan je oefeningen maken met een gesimuleerd systeem
+Het voorbeeld dat gegeven wordt is de hoogteregeling van een drone. 
+In volgende video wordt een versimpelde versie gegeven hoe zo een systeem kan werken.          
+"""
+)
+
+st.video('https://youtu.be/QNnx2evKb_0?si=0s1S4yWZuPnfQC0f')
+
+st.markdown("""
+De regelkring van het systeem ziet er als volgt uit. 
+De **setwaarde of referentiewaarde** wordt vergelijk met het **meetsignaal** van de *sensor*. In dit geval is de meetwaarde de hoogte van de drone in meter.
+Het resulterende **foutsignaal** zal dan door de *controler of regelaar* omgezet worden naar een **regelsignaal**.
+In dit geval is het regelsignaal het vermogen dat naar de motoren van de drone gaat.
+
+Het doel is om een systeem te hebben dat snel reageert en geen oscillatie vertoont.
+""")
+
+st.image('images/500px-Gesloten_regelsysteem.png')
 st.sidebar.success('Kies een oefening.')
--- pages/oefening_1.py
+++ pages/oefening_1.py
@@ -4,6 +4,20 @@ from simulated_PID_controller import PID_Controller, SMD_System_Sim
 
 st.set_page_config(page_title="Plotting Demo", page_icon="📈")
 
+st.markdown(
+"""
+
+1. Zet de Kp waarde op 1. Wat merk je op? Kijk hierbij naar:
+    * Hoe lang duurt het voordat de gewenste waarde bereikt wordt?
+    * Wordt de werkelijke waarde soms grote dan de referentiewaarde?
+    * Is er oscillatie in het systeem? 
+
+2. Zet de Kp Waarde op 10. Wat merk je op? Vergelijk met je antwoorden op vraag 1.
+3. Zet de Kp waarde op 100. Wat merk je op? Vergelijk met je antwoorden op vraag 1 en 2.
+4. Wat zou je nemen als de ideale waarde voor Kp?
+"""
+)
+
 # Begin main code
 kp = st.number_input('Waarde voor Kp', step=1)
 ki = 0

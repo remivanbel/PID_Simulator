469b6b40bed8fc896c395e5df8d50e18d3fabd5e
remivanbel <47861142+remivanbel@users.noreply.github.com>
2024-11-25 21:43:01 +0100

    Update oefening_4.py change system

--- pages/oefening_4.py
+++ pages/oefening_4.py
@@ -10,7 +10,7 @@ ki = 0
 Td = st.number_input('Waarde voor Td')
 # Create Controller object
 controller = PID_Controller(kp, ki, 0, Td=Td)
-system_simulator = SMD_System_Sim(mass = 10, k = 0.5, c = 10)
+system_simulator = SMD_System_Sim(mass = 1, k = 100, c = 2)
 
 p = make_chart(controller, system_simulator)
 

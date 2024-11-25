ba185b97fd578a8f8891688246c165d4793be30c
remivanbel <47861142+remivanbel@users.noreply.github.com>
2024-11-25 21:45:07 +0100

    Update oefening_5.py

--- pages/oefening_5.py
+++ pages/oefening_5.py
@@ -6,8 +6,8 @@ st.set_page_config(page_title="Plotting Demo", page_icon="📈")
 
 # Begin main code
 kp = st.number_input('Waarde voor Kp')
-Ti = st.number_input('Waarde voor Ti', value=10000)
-Td = st.number_input('Waarde voor Td', value=0)
+Ti = st.number_input('Waarde voor Ti', value=10000.0)
+Td = st.number_input('Waarde voor Td', value=0.0)
 # Create Controller object
 controller = PID_Controller(kp, Ti=Ti, Td=Td)
 system_simulator = SMD_System_Sim(mass = 1, k = 100, c = 2)
